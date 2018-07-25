af4826118205a6ad95688016ac0594eb716379f6
Jonathan <johnnyboycurtis@gmail.com>
2018-07-25 10:51:02 -0700

    organizing code

--- Chapter04/MyButtonApp.py
+++ Chapter04/MyButtonApp.py
@@ -13,44 +13,55 @@ class MyApp(wx.App):
         self.InitFrame()
     
     def InitFrame(self):
-        frame = MyForm()
-        frame.Show(True)
+        #frame = MyForm()
+        frame = MyFrame()
+        frame.Show()
 
 
-class MyForm(wx.Frame):
+class MyFrame(wx.Frame):
+    def __init__(self, title="MyButtonApp"):
+        super().__init__(None, title=title)
+        # initialize the frame's contents
+        self.OnInit()
 
-    def __init__(self):
-        wx.Frame.__init__(self, None, wx.ID_ANY, title='My Form')
+    def OnInit(self):
+        self.panel = MyForm(self)
+        #self.Show()
+        self.Fit()
+
+class MyForm(wx.Panel):
+
+    def __init__(self, parent):
+        super().__init__(parent=parent)
 
         # Add a panel so it looks correct on all platforms
-        self.panel = wx.Panel(self, wx.ID_ANY)
 
         bmp = wx.ArtProvider.GetBitmap(wx.ART_INFORMATION, wx.ART_OTHER, (16, 16))
-        titleIco = wx.StaticBitmap(self.panel, wx.ID_ANY, bmp)
-        title = wx.StaticText(self.panel, wx.ID_ANY, 'My Title')
+        titleIco = wx.StaticBitmap(self, wx.ID_ANY, bmp)
+        title = wx.StaticText(self, wx.ID_ANY, 'My Title')
 
         bmp = wx.ArtProvider.GetBitmap(wx.ART_TIP, wx.ART_OTHER, (16, 16))
-        inputOneIco = wx.StaticBitmap(self.panel, wx.ID_ANY, bmp)
-        labelOne = wx.StaticText(self.panel, wx.ID_ANY, 'Input 1')
-        inputTxtOne = wx.TextCtrl(self.panel, wx.ID_ANY, value='Text box')
+        inputOneIco = wx.StaticBitmap(self, wx.ID_ANY, bmp)
+        labelOne = wx.StaticText(self, wx.ID_ANY, 'Input 1')
+        inputTxtOne = wx.TextCtrl(self, wx.ID_ANY, value='Text box')
 
-        inputTwoIco = wx.StaticBitmap(self.panel, wx.ID_ANY, bmp)
-        labelTwo = wx.StaticText(self.panel, wx.ID_ANY, 'Input 2')
-        inputTxtTwo = wx.SpinCtrl(self.panel, wx.ID_ANY, value="0", min=0, max=100)
+        inputTwoIco = wx.StaticBitmap(self, wx.ID_ANY, bmp)
+        labelTwo = wx.StaticText(self, wx.ID_ANY, 'Input 2')
+        inputTxtTwo = wx.SpinCtrl(self, wx.ID_ANY, value="0", min=0, max=100)
 
-        inputThreeIco = wx.StaticBitmap(self.panel, wx.ID_ANY, bmp)
-        labelThree = wx.StaticText(self.panel, wx.ID_ANY, 'Input 3')
-        inputTxtThree = wx.Choice(self.panel, choices=['A', 'B', 'C'])
+        inputThreeIco = wx.StaticBitmap(self, wx.ID_ANY, bmp)
+        labelThree = wx.StaticText(self, wx.ID_ANY, 'Input 3')
+        inputTxtThree = wx.Choice(self, choices=['A', 'B', 'C'])
 
-        inputFourIco = wx.StaticBitmap(self.panel, wx.ID_ANY, bmp)
-        labelFour = wx.StaticText(self.panel, wx.ID_ANY, 'Input 4')
+        inputFourIco = wx.StaticBitmap(self, wx.ID_ANY, bmp)
+        labelFour = wx.StaticText(self, wx.ID_ANY, 'Input 4')
         #inputTxtFour = wx.TextCtrl(self.panel, wx.ID_ANY, '')
-        inputFour1 = wx.CheckBox(parent=self.panel, label="Choice 1")
-        inputFour2 = wx.CheckBox(parent=self.panel, label="Choice 2")
-        inputFour3 = wx.CheckBox(parent=self.panel, label="Choice 3")
+        inputFour1 = wx.CheckBox(parent=self, label="Choice 1")
+        inputFour2 = wx.CheckBox(parent=self, label="Choice 2")
+        inputFour3 = wx.CheckBox(parent=self, label="Choice 3")
 
-        okBtn = wx.Button(self.panel, wx.ID_ANY, 'OK')
-        cancelBtn = wx.Button(self.panel, wx.ID_ANY, 'Cancel')
+        okBtn = wx.Button(self, wx.ID_ANY, 'OK')
+        cancelBtn = wx.Button(self, wx.ID_ANY, 'Cancel')
         self.Bind(wx.EVT_BUTTON, self.onOK, okBtn)
         self.Bind(wx.EVT_BUTTON, self.onCancel, cancelBtn)
 
@@ -88,17 +99,17 @@ class MyForm(wx.Frame):
         btnSizer.Add(cancelBtn, 0, wx.ALL, 5)
 
         topSizer.Add(titleSizer, 0, wx.CENTER)
-        topSizer.Add(wx.StaticLine(self.panel,), 0, wx.ALL|wx.EXPAND, 5)
+        topSizer.Add(wx.StaticLine(self,), 0, wx.ALL|wx.EXPAND, 5)
         topSizer.Add(inputOneSizer, 0, wx.ALL|wx.EXPAND, 5)
         topSizer.Add(inputTwoSizer, 0, wx.ALL|wx.EXPAND, 5)
         topSizer.Add(inputThreeSizer, 0, wx.ALL|wx.EXPAND, 5)
         topSizer.Add(inputFourSizer, 0, wx.ALL|wx.EXPAND, 5)
-        topSizer.Add(wx.StaticLine(self.panel), 0, wx.ALL|wx.EXPAND, 5)
+        topSizer.Add(wx.StaticLine(self), 0, wx.ALL|wx.EXPAND, 5)
         topSizer.Add(btnSizer, 0, wx.ALL|wx.CENTER, 5)
 
-        self.panel.SetSizer(topSizer)
+        self.SetSizer(topSizer)
         topSizer.Fit(self)
-        self.panel.Layout()
+        self.Layout()
 
 
     def onOK(self, event):
@@ -109,7 +120,9 @@ class MyForm(wx.Frame):
         self.closeProgram()
 
     def closeProgram(self):
-        self.Close()
+        # self.GetParent() will get the frame which
+        # has the .Close() method to close the program
+        self.GetParent().Close()
 
 
 # Run the program
