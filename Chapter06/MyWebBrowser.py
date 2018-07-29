4024541ba0ff0bdb60c1a626b1267c5a10476fb3
Jonathan <johnnyboycurtis@gmail.com>
2018-07-29 10:02:01 -0700

    updates to browser methods

--- Chapter06/MyWebBrowser.py
+++ Chapter06/MyWebBrowser.py
@@ -2,45 +2,17 @@
 # Recipe 9: Surfing the web in your app
 #
 import wx
-import wx.html2 as html2
+from wx import html2
 
-class NaviBar(wx.Panel):
-    def __init__(self, parent, browser):
-        super(NaviBar, self).__init__(parent)
+class MyApp(wx.App):
+    def OnInit(self):
+        WebFrame(None, "Surfing the Web").Show()
+        return True
 
-        b = browser
-        print("Current URL:", b.GetCurrentURL())
-        self._url = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
-        self._url.SetHint("Enter URL here and press enter...")
-
-        back = wx.Button(self, style=wx.BU_EXACTFIT)
-        back.Bitmap = wx.ArtProvider.GetBitmap(wx.ART_GO_BACK, 
-                                               wx.ART_TOOLBAR)
-        fw = wx.Button(self, style=wx.BU_EXACTFIT)
-        fw.Bitmap = wx.ArtProvider.GetBitmap(wx.ART_GO_FORWARD,
-                                             wx.ART_TOOLBAR)
-
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(back, 0, wx.ALL, 5)
-        sizer.Add(fw, 0, wx.ALL, 5)
-        sizer.Add(self._url, 1, wx.EXPAND)
-        self.SetSizer(sizer)
-        
-        
-        self.Bind(wx.EVT_TEXT_ENTER, 
-                  lambda event: b.LoadURL(self._url.Value))
-        self.Bind(wx.EVT_BUTTON, lambda event: b.GoBack(), back)
-        self.Bind(wx.EVT_BUTTON, lambda event: b.GoForward(), fw)
-        self.Bind(wx.EVT_UPDATE_UI,
-                  lambda event: event.Enable(b.CanGoBack()),
-                  back)
-        self.Bind(wx.EVT_UPDATE_UI,
-                  lambda event: event.Enable(b.CanGoForward()),
-                  fw)
 
 class WebFrame(wx.Frame):
     def __init__(self, parent, title):
-        super(WebFrame, self).__init__(parent, title=title)
+        super().__init__(parent, title=title)
         
         self._browser = html2.WebView.New(self)
         self._browser.LoadURL("www.google.com") # home page
@@ -57,12 +29,45 @@ class WebFrame(wx.Frame):
     def OnTitle(self, event):
         self.Title = event.GetString()
 
-class MyApp(wx.App):
-    def OnInit(self):
-        WebFrame(None, "Surfing the Web").Show()
-        return True
+
+
+class NaviBar(wx.Panel):
+    def __init__(self, parent, browser):
+        super().__init__(parent)
+
+        self.browser = browser
+        print("Current URL:", self.browser.GetCurrentURL())
+        self._url = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
+        self._url.SetHint("Enter URL here and press enter...")
+
+        back = wx.Button(self, style=wx.BU_EXACTFIT)
+        back.Bitmap = wx.ArtProvider.GetBitmap(wx.ART_GO_BACK, 
+                                               wx.ART_TOOLBAR)
+        fw = wx.Button(self, style=wx.BU_EXACTFIT)
+        fw.Bitmap = wx.ArtProvider.GetBitmap(wx.ART_GO_FORWARD,
+                                             wx.ART_TOOLBAR)
+
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(back, 0, wx.ALL, 5)
+        sizer.Add(fw, 0, wx.ALL, 5)
+        sizer.Add(self._url, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+
+
+    def onEnter(self, event):
+        self.browser.LoadURL(self._url.Value)
+    
+    def goBack(self, event):
+        event.Enable(self.browser.CanGoBack())
+        self.browser.GoBack()
+
+    def goForward(self, event):
+        event.Enable(self.browser.CanGoForward())
+        self.browser.GoForward()
+
+
 
 if __name__ == "__main__":
-    app = MyApp(False)
+    app = MyApp()
     app.MainLoop()
     
